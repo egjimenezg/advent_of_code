4322531cd8212f394508e8eed24650998e03799e
Gamaliel Jimenez <egjimenezg@gmail.com>
2025-02-01 22:09:36 -0600

    feat: solution to part 2 of day 3

--- lib/2023/day3.ex
+++ lib/2023/day3.ex
@@ -1,10 +1,12 @@
 defmodule AdventOfCode2023.Day3 do
   def answer() do
-    File.read!("priv/day3_input.txt")
-    |> String.split("\n")
-    |> build_matrix()
-    |> find_part_numbers()
-    |> Enum.sum()
+    %{part_numbers: part_numbers, gears: gears} =
+      File.read!("priv/day3_input.txt")
+      |> String.split("\n")
+      |> build_matrix()
+      |> find_part_numbers_and_gears()
+
+    {Enum.sum(part_numbers), add_up_gear_ratios(gears)}
   end
 
   def build_matrix(file_content) do
@@ -25,49 +27,100 @@ defmodule AdventOfCode2023.Day3 do
     {matrix, file_content}
   end
 
-  def find_part_numbers({matrix, file_content}) do
+  def find_part_numbers_and_gears({matrix, file_content}) do
     reg_exp = ~r/\d+/
 
-    file_content
-    |> Enum.with_index()
-    |> Enum.map(fn {line, row} ->
-      {row, Regex.scan(reg_exp, line, return: :index)}
-    end)
-    |> Enum.reduce([], fn line_numbers, part_numbers ->
-      {row, numbers} = line_numbers
+    %{part_numbers: part_numbers, gears: gears} =
+      file_content
+      |> Stream.with_index()
+      |> Stream.map(fn {line, row} ->
+        {row, Regex.scan(reg_exp, line, return: :index)}
+      end)
+      |> Enum.reduce(%{part_numbers: [], gears: %{}}, fn {row, numbers}, part_numbers_and_gears ->
+        line_numbers_and_gears =
+          numbers
+          |> Stream.map(fn [{column, digits}] ->
+            part_number = get_part_number_from_matrix(row, column, digits, matrix)
+            adjacent_symbols = find_adjacent_symbols(row, column, digits, matrix)
+            {part_number, adjacent_symbols}
+          end)
+          |> Stream.filter(fn
+            {_, []} -> false
+            _ -> true
+          end)
+          |> Enum.reduce(%{part_numbers: [], gears: %{}}, fn {part_number, adjacent_symbols},
+                                                             part_numbers_and_gears_in_line ->
+            adjacent_gears =
+              Enum.filter(adjacent_symbols, &(Map.get(matrix, &1) == "*"))
+              |> Enum.map(fn row_column -> {row_column, [part_number]} end)
+              |> Map.new()
 
-      numbers
-      |> Enum.filter(fn [{number_column, digits}] ->
-        adjacent_to_symbol?({row, number_column, digits}, matrix)
+            part_numbers_and_gears_in_line
+            |> Map.update!(:part_numbers, fn part_numbers ->
+              [part_number | part_numbers]
+            end)
+            |> Map.update!(:gears, fn gears_in_line ->
+              Map.merge(gears_in_line, adjacent_gears, fn _k, v1, v2 -> v1 ++ v2 end)
+            end)
+          end)
+
+        part_numbers_and_gears
+        |> Map.update!(:part_numbers, fn part_numbers ->
+          line_numbers_and_gears
+          |> Map.get(:part_numbers)
+          |> Enum.concat(part_numbers)
+        end)
+        |> Map.update!(:gears, fn gears ->
+          line_numbers_and_gears
+          |> Map.get(:gears)
+          |> Map.merge(gears, fn _k, v1, v2 -> v1 ++ v2 end)
+        end)
       end)
-      |> Enum.map(fn [{number_column, digits}] ->
-        number_column..(number_column + digits - 1)
-        |> Enum.map_join("", &Map.get(matrix, {row, &1}))
-        |> String.to_integer()
-      end)
-      |> Enum.concat(part_numbers)
-    end)
+
+    gears =
+      Map.values(gears)
+      |> Enum.filter(fn gear -> length(gear) == 2 end)
+
+    %{part_numbers: part_numbers, gears: gears}
   end
 
-  defp adjacent_to_symbol?({number_row, number_column, number_of_digits}, matrix) do
-    (number_column - 1)..(number_column + number_of_digits)
-    |> Enum.any?(fn column ->
-      Enum.any?((number_row - 1)..(number_row + 1), fn row ->
+  defp find_adjacent_symbols(number_row, number_column, digits, matrix) do
+    columns = (number_column - 1)..(number_column + digits)
+
+    Enum.reduce(columns, [], fn column, symbols ->
+      rows = (number_row - 1)..(number_row + 1)
+
+      Stream.filter(rows, fn row ->
         matrix
         |> Map.get({row, column})
-        |> is_symbol()
+        |> symbol?()
       end)
+      |> Stream.map(fn row -> {row, column} end)
+      |> Enum.concat(symbols)
     end)
   end
 
-  defp is_symbol(nil), do: false
-  defp is_symbol(character) when character == ".", do: false
+  defp symbol?(nil), do: false
+  defp symbol?(character) when character == ".", do: false
 
-  defp is_symbol(maybe_number) do
+  defp symbol?(maybe_number) do
     Integer.parse(maybe_number)
     |> case do
       {_, ""} -> false
       :error -> true
     end
+  end
+
+  defp get_part_number_from_matrix(row, column, digits, matrix) do
+    column..(column + digits - 1)
+    |> Enum.map_join("", &Map.get(matrix, {row, &1}))
+    |> String.to_integer()
+  end
+
+  defp add_up_gear_ratios(gears) do
+    Enum.reduce(gears, 0, fn gear, gear_ratios_sum ->
+      [part_num_a, part_num_b] = gear
+      gear_ratios_sum + part_num_a * part_num_b
+    end)
   end
 end
--- test/2023/day3_test.exs
+++ test/2023/day3_test.exs
@@ -23,35 +23,50 @@ defmodule AdventOfCode2023.Day3Test do
            )
   end
 
-  test "find_part_numbers/1 should return the part numbers in the file" do
-    file_content = [
-      "467..114..",
-      "...*......"
-    ]
+  describe "part numbers" do
+    test "find_part_numbers_and_gears/1 should return the part numbers in the file" do
+      file_content = [
+        "467..114..",
+        "...*......"
+      ]
 
-    matrix = %{
-      {0, 0} => "4",
-      {0, 1} => "6",
-      {0, 2} => "7",
-      {0, 3} => ".",
-      {0, 4} => ".",
-      {0, 5} => "1",
-      {0, 6} => "1",
-      {0, 7} => "4",
-      {0, 8} => ".",
-      {0, 9} => ".",
-      {1, 0} => ".",
-      {1, 1} => ".",
-      {1, 2} => ".",
-      {1, 3} => "*",
-      {1, 4} => ".",
-      {1, 5} => ".",
-      {1, 6} => ".",
-      {1, 7} => ".",
-      {1, 8} => ".",
-      {1, 9} => "."
-    }
+      matrix = %{
+        {0, 0} => "4",
+        {0, 1} => "6",
+        {0, 2} => "7",
+        {0, 3} => ".",
+        {0, 4} => ".",
+        {0, 5} => "1",
+        {0, 6} => "1",
+        {0, 7} => "4",
+        {0, 8} => ".",
+        {0, 9} => ".",
+        {1, 0} => ".",
+        {1, 1} => ".",
+        {1, 2} => ".",
+        {1, 3} => "*",
+        {1, 4} => ".",
+        {1, 5} => ".",
+        {1, 6} => ".",
+        {1, 7} => ".",
+        {1, 8} => ".",
+        {1, 9} => "."
+      }
 
-    assert [467] == Day3.find_part_numbers({matrix, file_content})
+      assert %{part_numbers: [467]} = Day3.find_part_numbers_and_gears({matrix, file_content})
+    end
+
+    test "find_part_numbers_and_gears/1 should return the gears" do
+      file_content = [
+        "467.",
+        "...*.",
+        "..35"
+      ]
+
+      {matrix, _} = Day3.build_matrix(file_content)
+
+      assert %{gears: [[35, 467]], part_numbers: [35, 467]} ==
+               Day3.find_part_numbers_and_gears({matrix, file_content})
+    end
   end
 end
