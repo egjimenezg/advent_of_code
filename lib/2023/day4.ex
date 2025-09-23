52e0fd57088bd60a75611033b29d583e858f9d1e
Gamaliel Jimenez <egjimenezg@gmail.com>
2025-09-23 01:44:50 -0600

    Solution to part 2 of day 4

--- lib/2023/day4.ex
+++ lib/2023/day4.ex
@@ -1,13 +1,61 @@
-defmodule AdventOfCode2024.Day4 do
+defmodule AdventOfCode2023.Day4 do
   def answer() do
     File.stream!("priv/day4_input.txt")
     |> Stream.map(&get_winning_and_owned_numbers/1)
-    |> Stream.map(&calculate_card_points/1)
-    |> Enum.sum()
+    |> Stream.map(&calculate_number_of_copies_won/1)
+    |> Stream.map(&get_card_copies/1)
+    |> Enum.reduce(%{}, fn {card_number, items}, acc ->
+      Map.put(acc, card_number, items)
+    end)
+    |> count_cards()
+  end
+
+  def sum_values(card_number, cards, memory) do
+    case Map.get(memory, card_number) do
+      nil ->
+        Map.get(cards, card_number)
+        |> case do
+          card_copies when map_size(card_copies) == 0 ->
+            {Map.put(memory, card_number, 0), 0}
+
+          card_copies ->
+            values_sum =
+              card_copies
+              |> Map.values()
+              |> Enum.sum()
+
+            {memory, total} =
+              card_copies
+              |> Enum.reduce({memory, 0}, fn {k, _}, {memory, acc} ->
+                {memory, value} = sum_values(k, cards, memory)
+                {memory, acc + value}
+              end)
+
+            total = total + values_sum
+            {Map.put(memory, card_number, total), total}
+        end
+
+      value ->
+        {memory, value}
+    end
+  end
+
+  def count_cards(cards) do
+    numbers = Map.keys(cards)
+
+    {sum, _} =
+      numbers
+      |> Enum.reduce({0, %{}}, fn card_number, {acc, memory} ->
+        {memory, value} = sum_values(card_number, cards, memory)
+        {acc + value, memory}
+      end)
+
+    sum + length(numbers)
   end
 
   def get_winning_and_owned_numbers(line) do
     [winning_numbers, owned_numbers] = String.split(line, "|")
+    [card_number] = Regex.run(~r/\d+/, winning_numbers)
 
     winning_numbers =
       Regex.replace(~r/Card\s+\d+:/, winning_numbers, "")
@@ -15,24 +63,41 @@ defmodule AdventOfCode2024.Day4 do
 
     owned_numbers = get_numbers_from_str(owned_numbers)
 
-    {winning_numbers, owned_numbers}
+    %{
+      card_number: String.to_integer(card_number),
+      winning_numbers: winning_numbers,
+      owned_numbers: owned_numbers
+    }
   end
 
-  def calculate_card_points({winning_numbers, owned_numbers})
-      when is_list(winning_numbers) and is_list(owned_numbers) do
+  def calculate_number_of_copies_won(%{
+        card_number: card_number,
+        winning_numbers: winning_numbers,
+        owned_numbers: owned_numbers
+      }) do
     winning_numbers = MapSet.new(winning_numbers)
     owned_numbers = MapSet.new(owned_numbers)
 
-    MapSet.intersection(winning_numbers, owned_numbers)
-    |> MapSet.to_list()
-    |> Enum.count()
-    |> case do
-      cards_number when cards_number > 0 ->
-        2 ** (cards_number - 1)
+    copies_won =
+      MapSet.intersection(winning_numbers, owned_numbers)
+      |> MapSet.to_list()
+      |> Enum.count()
 
-      _ ->
-        0
-    end
+    %{card_number: card_number, copies_won: copies_won}
+  end
+
+  def get_card_copies(%{card_number: card_number, copies_won: copies_won}) do
+    copies =
+      case copies_won do
+        copies_won when copies_won > 0 ->
+          (card_number + 1)..(card_number + copies_won)
+          |> Enum.reduce(%{}, fn elem, acc -> Map.put(acc, elem, 1) end)
+
+        _ ->
+          %{}
+      end
+
+    {card_number, copies}
   end
 
   defp get_numbers_from_str(str) when is_binary(str) do
--- test/2023/day4_test.exs
+++ test/2023/day4_test.exs
@@ -1,19 +1,36 @@
-defmodule AdventOfCode2024.Day4Test do
-  use ExUnit.Case
+defmodule AdventOfCode2023.Day4Test do
+  use ExUnit.Case, async: true
 
-  alias AdventOfCode2024.Day4
+  alias AdventOfCode2023.Day4
 
   test "parses card data to return winning and owned numbers" do
     card = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-    {winning_numbers, owned_numbers} = Day4.get_winning_and_owned_numbers(card)
+
+    %{winning_numbers: winning_numbers, owned_numbers: owned_numbers} =
+      Day4.get_winning_and_owned_numbers(card)
+
     assert winning_numbers == [41, 48, 83, 86, 17]
     assert owned_numbers == [83, 86, 6, 31, 17, 9, 48, 53]
   end
 
-  test "calculates card points" do
+  test "should calculates number of copies won" do
+    card_number = 1
     winning_numbers = [41, 48, 83, 86, 17]
     owned_numbers = [83, 86, 6, 31, 17, 9, 48, 53]
-    card_points = Day4.calculate_card_points({winning_numbers, owned_numbers})
-    assert card_points == 8
+
+    %{card_number: card_number, copies_won: copies_won} =
+      Day4.calculate_number_of_copies_won(%{
+        card_number: card_number,
+        winning_numbers: winning_numbers,
+        owned_numbers: owned_numbers
+      })
+
+    assert card_number == 1
+    assert copies_won == 4
+  end
+
+  test "should get the card copies" do
+    card_copies = Day4.get_card_copies(%{card_number: 1, copies_won: 4})
+    assert match?({1, %{2 => 1, 3 => 1, 4 => 1, 5 => 1}}, card_copies)
   end
 end
